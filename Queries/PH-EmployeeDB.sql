1097efa376c259a98867b55e8967429dbffee016
Nadiezhda Hernandez Bonilla <nadiezhdamhb@gmail.com>
2022-05-29 23:21:44 -0700

    updating files

--- Queries/PH-EmployeeDB.sql
+++ Queries/PH-EmployeeDB.sql
@@ -1,4 +1,3 @@
--- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -31,21 +30,5 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title VARCHAR NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
-CREATE TABLE Dept_Emp (
-	dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
-SELECT * FROM titles ;
+
+SELECT * FROM salaries;
